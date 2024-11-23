f757f5c3d61b958171e48957489d5c811f8591ad
Luis Suarez <luisfreiser@gmail.com>
2024-11-23 17:49:59 -0500

    update route.ts

--- app/api/pedidos/[id]/route.ts
+++ app/api/pedidos/[id]/route.ts
@@ -1,32 +1,60 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import { prisma } from "@/lib/prisma";
+
+// type Props = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// export async function PATCH(request: NextRequest, context: Props) {
+//   try {
+//     const { estado } = await request.json();
+//     const { id } = context.params;
+
+//     // Actualizar el estado del pedido
+//     const pedidoActualizado = await prisma.pedido.update({
+//       where: { id: parseInt(id) },
+//       data: { estado },
+//     });
+
+//     return NextResponse.json(pedidoActualizado, { status: 200 });
+//   } catch (error) {
+//     console.error("Error al actualizar el pedido:", error);
+//     return NextResponse.json(
+//       { error: "Error al actualizar el pedido" },
+//       { status: 500 }
+//     );
+//   } finally {
+//     // Es buena práctica desconectar el cliente de Prisma
+//     await prisma.$disconnect();
+//   }
+// }
+
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export async function PATCH(request: NextRequest, context: Props) {
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse> {
   try {
-    const { estado } = await request.json();
-    const { id } = context.params;
+    const { estado } = await req.json();
+    const id = params.id;
 
-    // Actualizar el estado del pedido
     const pedidoActualizado = await prisma.pedido.update({
-      where: { id: parseInt(id) },
+      where: {
+        id: parseInt(id),
+      },
       data: { estado },
     });
 
-    return NextResponse.json(pedidoActualizado, { status: 200 });
+    return NextResponse.json(pedidoActualizado);
   } catch (error) {
     console.error("Error al actualizar el pedido:", error);
     return NextResponse.json(
       { error: "Error al actualizar el pedido" },
       { status: 500 }
     );
-  } finally {
-    // Es buena práctica desconectar el cliente de Prisma
-    await prisma.$disconnect();
   }
 }
